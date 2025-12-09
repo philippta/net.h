b5a5f07f55b25b4118a178387b03fdc9d2b1dfb6
Philipp Tanlak <philipp.tanlak@gmail.com>
2025-12-09 21:11:18 +0100

    events

--- net.h
+++ net.h
@@ -1,3 +1,6 @@
+#ifndef NET_H
+#define NET_H
+
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -5,145 +8,232 @@
 #include <netinet/tcp.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/event.h>
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#define NET_DIAL_NONE      0x00
-#define NET_DIAL_NONBLOCK  0x01
-#define NET_DIAL_NODELAY   0x02
-#define NET_DIAL_KEEPALIVE 0x04
+#define TCP_CONNECT_NONE      0x00
+#define TCP_CONNECT_NONBLOCK  0x01
+#define TCP_CONNECT_NODELAY   0x02
+#define TCP_CONNECT_KEEPALIVE 0x04
 
-#define NET_LISTEN_NONE      0x00
-#define NET_LISTEN_NONBLOCK  0x01
-#define NET_LISTEN_REUSEADDR 0x02
-#define NET_LISTEN_REUSEPORT 0x03
+#define TCP_LISTEN_NONE      0x00
+#define TCP_LISTEN_NONBLOCK  0x01
+#define TCP_LISTEN_REUSEADDR 0x02
+#define TCP_LISTEN_REUSEPORT 0x03
 
-#define NET_ADDR_STRLEN INET6_ADDRSTRLEN
+int tcpaccept(int fd);
+int tcpconnect(const char *addr, unsigned short port, int flags);
+int tcplisten(const char *addr, unsigned short port, int flags);
 
-int net_accept(int fd);
-int net_addr(int fd, char *ip, size_t iplen, unsigned short *port);
-int net_dial_tcp(const char *addr, unsigned short port, int flags);
-int net_listen_tcp(const char *addr, unsigned short port, int flags);
-int net_resolve(const char *host, struct sockaddr_in *addr);
+#define IP_ADDR_STRLEN INET6_ADDRSTRLEN
 
-int net_dial_tcp(const char *addr, unsigned short port, int flags) {
-    int                fd, ret;
-    struct sockaddr_in sockaddr = {};
+int ipaddr(int fd, char *ip, size_t iplen, unsigned short *port);
+int ipresolve(const char *host, struct sockaddr_in *addr);
 
-    ret = net_resolve(addr, &sockaddr);
-    if (ret < 0) return -1;
+void fdnonblock(int fd);
 
-    sockaddr.sin_family = AF_INET;
-    sockaddr.sin_port   = htons(port);
+struct event {
+        int   fd;
+        int   events;
+        void *data;
+};
 
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) return -1;
+int evinit(void);
+int evread(int ev, int fd, void *data);
+int evwrite(int ev, int fd, void *data);
+int evisread(struct event *ev);
+int eviswrite(struct event *ev);
+int evwait(int ev, struct event *changes, int nchanges);
 
-    if (flags & NET_DIAL_NONBLOCK) {
+#ifndef EVWAIT_KQUEUE_SIZE
+#define EVWAIT_KQUEUE_SIZE 128
+#endif
+
+// #define NET_IMPLEMENTATION
+#ifdef NET_IMPLEMENTATION
+
+int tcpconnect(const char *addr, unsigned short port, int flags) {
+        int                fd, ret;
+        struct sockaddr_in sockaddr = {};
+
+        ret = ipresolve(addr, &sockaddr);
+        if (ret < 0)
+                return -1;
+
+        sockaddr.sin_family = AF_INET;
+        sockaddr.sin_port   = htons(port);
+
+        fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd < 0)
+                return -1;
+
+        if (flags & TCP_CONNECT_NONBLOCK) {
+                int fl = fcntl(fd, F_GETFL, 0);
+                fcntl(fd, F_SETFL, fl | O_NONBLOCK);
+        }
+
+        if (flags & TCP_CONNECT_NODELAY) {
+                int on = 1;
+                setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
+        }
+
+        if (flags & TCP_CONNECT_KEEPALIVE) {
+                int on = 1;
+                setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &on, sizeof(on));
+        }
+
+        int on = 1;
+        setsockopt(fd, SOL_SOCKET, SO_LINGER, &on, sizeof(on));
+
+        ret = connect(fd, (struct sockaddr *)&sockaddr, sizeof(sockaddr));
+        if (ret < 0 && errno != EINPROGRESS) {
+                close(fd);
+                return -1;
+        }
+
+        return fd;
+}
+
+int tcplisten(const char *addr, unsigned short port, int flags) {
+        int                fd, ret;
+        struct sockaddr_in sockaddr = {};
+
+        ret = ipresolve(addr, &sockaddr);
+        if (ret < 0)
+                return -1;
+
+        sockaddr.sin_family = AF_INET;
+        sockaddr.sin_port   = htons(port);
+
+        fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd < 0)
+                return -1;
+
+        if (flags & TCP_LISTEN_NONBLOCK) {
+                int fl = fcntl(fd, F_GETFL, 0);
+                fcntl(fd, F_SETFL, fl | O_NONBLOCK);
+        }
+
+        if (flags & TCP_LISTEN_REUSEADDR) {
+                int on = 1;
+                setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+        }
+
+        if (flags & TCP_LISTEN_REUSEPORT) {
+                int on = 1;
+                setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
+        }
+
+        ret = bind(fd, (struct sockaddr *)&sockaddr, sizeof(sockaddr));
+        if (ret < 0) {
+                close(fd);
+                return -1;
+        }
+
+        ret = listen(fd, 128);
+        if (ret < 0) {
+                close(fd);
+                return -1;
+        }
+
+        return fd;
+}
+
+int tcpaccept(int fd) {
+        return accept(fd, NULL, 0);
+}
+
+int ipaddr(int fd, char *ip, size_t iplen, unsigned short *port) {
+        int                     ret;
+        struct sockaddr_storage addr;
+        socklen_t               addrlen;
+
+        ret = getpeername(fd, (struct sockaddr *)&addr, &addrlen);
+        if (ret < 0)
+                return -1;
+
+        if (addr.ss_family == AF_INET) {
+                struct sockaddr_in *s4 = (struct sockaddr_in *)&addr;
+                inet_ntop(AF_INET, &s4->sin_addr, ip, iplen);
+                *port = ntohs(s4->sin_port);
+        } else if (addr.ss_family == AF_INET6) {
+                struct sockaddr_in6 *s6 = (struct sockaddr_in6 *)&addr;
+                inet_ntop(AF_INET6, &s6->sin6_addr, ip, iplen);
+                *port = ntohs(s6->sin6_port);
+        }
+
+        return 0;
+}
+
+int ipresolve(const char *host, struct sockaddr_in *addr) {
+        int              ret;
+        struct addrinfo  hints = {};
+        struct addrinfo *res   = NULL;
+
+        hints.ai_family = AF_INET;
+
+        ret = getaddrinfo(host, NULL, &hints, &res);
+        if (ret < 0)
+                return -1;
+
+        memcpy(addr, res->ai_addr, sizeof(struct sockaddr_in));
+
+        freeaddrinfo(res);
+        return 0;
+}
+
+void fdnonblock(int fd) {
         int fl = fcntl(fd, F_GETFL, 0);
         fcntl(fd, F_SETFL, fl | O_NONBLOCK);
-    }
-
-    if (flags & NET_DIAL_NODELAY) {
-        int on = 1;
-        setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
-    }
-
-    if (flags & NET_DIAL_KEEPALIVE) {
-        int on = 1;
-        setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &on, sizeof(on));
-    }
-
-    int on = 1;
-    setsockopt(fd, SOL_SOCKET, SO_LINGER, &on, sizeof(on));
-
-    ret = connect(fd, (struct sockaddr *)&sockaddr, sizeof(sockaddr));
-    if (ret < 0 && errno != EINPROGRESS) {
-        close(fd);
-        return -1;
-    }
-
-    return fd;
 }
 
-int net_listen_tcp(const char *addr, unsigned short port, int flags) {
-    int                fd, ret;
-    struct sockaddr_in sockaddr = {};
-
-    ret = net_resolve(addr, &sockaddr);
-    if (ret < 0) return -1;
-
-    sockaddr.sin_family = AF_INET;
-    sockaddr.sin_port   = htons(port);
-
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) return -1;
-
-    if (flags & NET_LISTEN_NONBLOCK) {
-        int fl = fcntl(fd, F_GETFL, 0);
-        fcntl(fd, F_SETFL, fl | O_NONBLOCK);
-    }
-
-    if (flags & NET_LISTEN_REUSEADDR) {
-        int on = 1;
-        setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-    }
-
-    if (flags & NET_LISTEN_REUSEPORT) {
-        int on = 1;
-        setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
-    }
-
-    ret = bind(fd, (struct sockaddr *)&sockaddr, sizeof(sockaddr));
-    if (ret < 0) {
-        close(fd);
-        return -1;
-    }
-
-    ret = listen(fd, 128);
-    if (ret < 0) {
-        close(fd);
-        return -1;
-    }
-
-    return fd;
+int evinit(void) {
+        return kqueue();
 }
 
-int net_resolve(const char *host, struct sockaddr_in *addr) {
-    int              ret;
-    struct addrinfo  hints = {};
-    struct addrinfo *res   = NULL;
-
-    hints.ai_family = AF_INET;
-
-    ret = getaddrinfo(host, NULL, &hints, &res);
-    if (ret < 0) return -1;
-
-    memcpy(addr, res->ai_addr, sizeof(struct sockaddr_in));
-
-    freeaddrinfo(res);
-    return 0;
+int evread(int ev, int fd, void *data) {
+        struct kevent event;
+        EV_SET(&event, fd, EVFILT_READ, EV_ADD, 0, 0, data);
+        return kevent(ev, &event, 1, NULL, 0, NULL);
 }
 
-int net_accept(int fd) { return accept(fd, NULL, 0); }
-
-int net_addr(int fd, char *ip, size_t iplen, unsigned short *port) {
-    int                     ret;
-    struct sockaddr_storage addr;
-    socklen_t               addrlen;
-
-    ret = getpeername(fd, (struct sockaddr *)&addr, &addrlen);
-    if (ret < 0) return -1;
-
-    if (addr.ss_family == AF_INET) {
-        struct sockaddr_in *s4 = (struct sockaddr_in *)&addr;
-        inet_ntop(AF_INET, &s4->sin_addr, ip, iplen);
-        *port = ntohs(s4->sin_port);
-    } else if (addr.ss_family == AF_INET6) {
-        struct sockaddr_in6 *s6 = (struct sockaddr_in6 *)&addr;
-        inet_ntop(AF_INET6, &s6->sin6_addr, ip, iplen);
-        *port = ntohs(s6->sin6_port);
-    }
-
-    return 0;
+int evwrite(int ev, int fd, void *data) {
+        struct kevent event;
+        EV_SET(&event, fd, EVFILT_WRITE, EV_ADD, 0, 0, data);
+        return kevent(ev, &event, 1, NULL, 0, NULL);
 }
+
+int evisread(struct event *ev) {
+        return ev->events & EVFILT_READ & 0x01;
+}
+
+int eviswrite(struct event *ev) {
+        return ev->events & EVFILT_WRITE & 0x01;
+}
+
+int evwait(int ev, struct event *changes, int nchanges) {
+        int           n;
+        struct kevent kevs[EVWAIT_KQUEUE_SIZE];
+
+        if (nchanges > EVWAIT_KQUEUE_SIZE) {
+                nchanges = EVWAIT_KQUEUE_SIZE;
+        }
+
+        n = kevent(ev, NULL, 0, kevs, nchanges, NULL);
+        if (n < 0) {
+                return n;
+        }
+
+        for (int i = 0; i < n; i++) {
+                changes[i].fd     = kevs[i].ident;
+                changes[i].events = kevs[i].filter;
+                changes[i].data   = kevs[i].udata;
+        }
+        return n;
+}
+
+#endif /* NET_IMPLEMENTATION */
+#endif /* NET_H */
--- test.c
+++ test.c
@@ -1,82 +1,152 @@
+#define NET_IMPLEMENTATION
 #include "net.h"
 
 void test_client_server() {
-    int            ln, srv, client, ret, n;
-    char           client_ip[NET_ADDR_STRLEN] = {};
-    unsigned short client_port;
-    char           server_ip[NET_ADDR_STRLEN] = {};
-    unsigned short server_port;
-    char           buf[64] = {};
+        int            ln, srv, client, ret, n;
+        char           client_ip[IP_ADDR_STRLEN] = {};
+        unsigned short client_port;
+        char           server_ip[IP_ADDR_STRLEN] = {};
+        unsigned short server_port;
+        char           buf[64] = {};
 
-    ln = net_listen_tcp("0.0.0.0", 8080, NET_LISTEN_REUSEADDR);
-    if (ln < 0) {
-        perror("net_listen_tcp");
-        return;
-    }
-
-    srv = net_dial_tcp("0.0.0.0", 8080, NET_DIAL_NONBLOCK);
-    if (srv < 0) {
-        perror("net_dial_tcp");
-        return;
-    }
-
-    client = net_accept(ln);
-    if (client < 0) {
-        perror("net_accept");
-        return;
-    }
-
-    ret = net_addr(client, client_ip, sizeof(client_ip), &client_port);
-    if (ret < 0) {
-        perror("net_addr client");
-        return;
-    }
-    ret = net_addr(srv, server_ip, sizeof(server_ip), &server_port);
-    if (ret < 0) {
-        perror("net_addr srv");
-        return;
-    }
-
-    printf("client: %s:%d\n", client_ip, client_port);
-    printf("server: %s:%d\n", server_ip, server_port);
-
-    for (;;) {
-        n = write(srv, "HELLO", 5);
-        printf("client->server write:%d\n", n);
-        if (n > 0) break;
-        usleep(10000);
-    }
-
-    for (;;) {
-        n = read(client, buf, sizeof(buf));
-        printf("client->server read:%d\n", n);
-        if (n > 0) {
-            printf("client->server msg:%.*s\n", n, buf);
-            break;
+        ln = tcplisten("0.0.0.0", 8080, TCP_LISTEN_REUSEADDR);
+        if (ln < 0) {
+                perror("tcplisten");
+                return;
         }
-        usleep(10000);
-    }
 
-    for (;;) {
-        n = write(client, "WORLD", 5);
-        printf("server->client write:%d\n", n);
-        if (n > 0) break;
-        usleep(10000);
-    }
-
-    for (;;) {
-        n = read(srv, buf, sizeof(buf));
-        printf("server->client read:%d\n", n);
-        if (n > 0) {
-            printf("server->client msg:%.*s\n", n, buf);
-            break;
+        srv = tcpconnect("0.0.0.0", 8080, TCP_CONNECT_NONBLOCK);
+        if (srv < 0) {
+                perror("tcpdial");
+                return;
         }
-        usleep(10000);
-    }
 
-    close(client);
-    close(srv);
-    close(ln);
+        client = tcpaccept(ln);
+        if (client < 0) {
+                perror("tcpaccept");
+                return;
+        }
+
+        ret = ipaddr(client, client_ip, sizeof(client_ip), &client_port);
+        if (ret < 0) {
+                perror("ipaddr client");
+                return;
+        }
+        ret = ipaddr(srv, server_ip, sizeof(server_ip), &server_port);
+        if (ret < 0) {
+                perror("ipaddr srv");
+                return;
+        }
+
+        printf("client: %s:%d\n", client_ip, client_port);
+        printf("server: %s:%d\n", server_ip, server_port);
+
+        for (;;) {
+                n = write(srv, "HELLO", 5);
+                printf("client->server write:%d\n", n);
+                if (n > 0)
+                        break;
+                usleep(10000);
+        }
+
+        for (;;) {
+                n = read(client, buf, sizeof(buf));
+                printf("client->server read:%d\n", n);
+                if (n > 0) {
+                        printf("client->server msg:%.*s\n", n, buf);
+                        break;
+                }
+                usleep(10000);
+        }
+
+        for (;;) {
+                n = write(client, "WORLD", 5);
+                printf("server->client write:%d\n", n);
+                if (n > 0)
+                        break;
+                usleep(10000);
+        }
+
+        for (;;) {
+                n = read(srv, buf, sizeof(buf));
+                printf("server->client read:%d\n", n);
+                if (n > 0) {
+                        printf("server->client msg:%.*s\n", n, buf);
+                        break;
+                }
+                usleep(10000);
+        }
+
+        close(client);
+        close(srv);
+        close(ln);
 }
 
-int main() { test_client_server(); }
+int freeidx(int *arr, int count) {
+        for (int i = 0; i < count; i++) {
+                if (arr[i] == 0) {
+                        return i;
+                }
+        }
+        return -1;
+}
+
+void test_chat() {
+        int           ln, ret, ev, n, idx, c;
+        int           cfds[128] = {};
+        struct event *e;
+        struct event  events[10];
+
+        ln = tcplisten("localhost", 8080, TCP_LISTEN_REUSEADDR | TCP_LISTEN_NONBLOCK);
+
+        ev = evinit();
+        if (ev < 0) {
+                perror("evinit");
+                return;
+        }
+
+        ret = evread(ev, ln, NULL);
+        if (ev < 0) {
+                perror("evread");
+                return;
+        }
+
+        for (;;) {
+                n = evwait(ev, events, 10);
+                for (int i = 0; i < n; i++) {
+                        e = &events[i];
+                        printf("fd:%d events:%d data:%p isread:%d iswrite:%d\n", e->fd, e->events, e->data, evisread(e),
+                               eviswrite(e));
+
+                        if (e->fd == ln) {
+                                c   = tcpaccept(e->fd);
+                                idx = freeidx(cfds, 128);
+                                if (idx < 0) {
+                                        close(c);
+                                } else {
+                                        cfds[idx] = c;
+                                        fdnonblock(c);
+                                }
+                                evread(ev, c, NULL);
+                        } else {
+                                char buf[128];
+                                int  rn = read(e->fd, buf, sizeof(buf));
+                                printf("%d: %.*s", e->fd, rn, buf);
+
+                                for (int j = 0; j < 128; j++) {
+                                        if (cfds[j] > 0 && cfds[j] != e->fd) {
+                                                char num[8] = {};
+                                                int  len    = snprintf(num, 8, "%d: ", cfds[j]);
+                                                write(cfds[j], num, len);
+                                                write(cfds[j], buf, rn);
+                                        }
+                                }
+                        }
+                }
+        }
+}
+
+int main() {
+        test_client_server();
+        // test_chat();
+}
